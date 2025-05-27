5eaa3a6c5152a90e0e536f9f89ebbc006f32b71d
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-05-27 22:49:59 +0900

    feat: 댓글 Entity, Dto 작성

--- src/main/java/org/example/newsfeed/comment/dto/CreateCommentRequestDto.java
+++ src/main/java/org/example/newsfeed/comment/dto/CreateCommentRequestDto.java
@@ -1,4 +1,10 @@
 package org.example.newsfeed.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
 public class CreateCommentRequestDto {
+    @NotBlank(message = "내용은 필수 입력값입니다")
+    private String content;
 }
