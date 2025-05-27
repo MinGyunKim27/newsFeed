fce4c9edd174dfdd892e2e080558e5c96c318c2e
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-05-27 20:19:07 +0900

    feat: 댓글기능 기본구조 생성

---
+++ src/main/java/org/example/newsfeed/comment/service/CommentService.java
@@ -0,0 +1,4 @@
+package org.example.newsfeed.comment.service;
+
+public interface CommentService {
+}
---
+++ src/main/java/org/example/newsfeed/comment/service/CommentServiceImpl.java
@@ -0,0 +1,9 @@
+package org.example.newsfeed.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.comment.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService{
+}
