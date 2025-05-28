6f95728dd0d4a31bbecdf455cb6cf45e866100b4
TaeHun Im <luxlaetitia9542@gmail.com>
2025-05-28 12:52:54 +0900

    ㅠã포feat : ãí 포스트 컨트롤러 구현

---
+++ src/main/java/org/example/newsfeed/post/controller/PostController.java
@@ -0,0 +1,35 @@
+package org.example.newsfeed.post.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.post.dto.CreatePostRequestDto;
+import org.example.newsfeed.post.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Long> createPost(
+            @RequestBody CreatePostRequestDto dto,
+            HttpServletRequest request
+            ) {
+
+        // 필터에서 JWT 검증하고 인증된 User 객체를 직접 request에 넣어준다.
+        User user =(User) request.getAttribute("user");
+
+        Long postId = postService.createPost(dto, user);
+
+        return ResponseEntity.ok(postId);
+    }
+
+
+}
--- src/main/java/org/example/newsfeed/post/service/PostServiceImpl.java
+++ src/main/java/org/example/newsfeed/post/service/PostServiceImpl.java
@@ -1,4 +1,32 @@
 package org.example.newsfeed.post.service;
 
-public class PostServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.example.newsfeed.post.dto.CreatePostRequestDto;
+import org.example.newsfeed.post.entitiy.Post;
+import org.example.newsfeed.post.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService{
+
+    private  final PostRepository postRepository;
+
+    @Override
+    public Long createPost(CreatePostRequestDto dto, User user) {
+
+        // 저장할 포스트 엔티티 생성
+        Post post = Post.builder()
+                .user(user)
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .imageUrl(dto.getImageUrl())
+                .build();
+
+        // 저장
+        postRepository.save(post);
+
+        // id값 반환
+        return post.getId();
+    }
 }
