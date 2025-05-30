0736e8ccf449c4e3c7ff581b7ad733493d075e8f
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-05-30 16:40:55 +0900

    feat: 댓글의 총 갯수만 반환하는 API 기능 추가

--- src/main/java/org/example/newsfeed/comment/repository/CommentRepository.java
+++ src/main/java/org/example/newsfeed/comment/repository/CommentRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findAllByPostIdOrderByCreatedAtDesc(Long postId, Pageable pageable);
+
+    Long countByPostId(Long post_id);
 }
--- src/main/java/org/example/newsfeed/comment/service/CommentService.java
+++ src/main/java/org/example/newsfeed/comment/service/CommentService.java
@@ -13,4 +13,6 @@ public interface CommentService {
     CommentResponseDto updateComment(@NotBlank(message = "내용은 필수 입력값입니다") String content, Long commentId, Long userId);
 
     void deleteComment(Long commentId, Long userId);
+
+    Long getCommentTotalCount(Long postId);
 }
